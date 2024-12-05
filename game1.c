8b78fc8147c1d3bff34e4705b94b819aaa7f4210
wingFire-29 <139781718+wingFire-29@users.noreply.github.com>
2024-12-06 00:27:09 +0530

    Add files via upload

---
+++ game1.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+
+int main(){
+    int num,guess , nguess=1;
+ 
+        nguess = 0;
+        srand(time(0));
+        num=rand()%100;//will genrate random number from 1 to 100
+        //printf("the number is %d\n\n",num);//it will not show which number is generated
+
+        do{
+            printf("guess the number\n\n");
+            scanf("%d",&guess);
+
+
+            if (guess>num)
+            {
+                printf("lower number please\n\n");
+            }
+            else if(guess <num)
+            {
+            printf("higher number please\n\n");
+
+            }
+            else 
+            {
+                printf("You guessed it right in %d attempt\n\n",nguess);
+            }
+            nguess++;
+
+        }while(guess!=num);
+
+
+    }
+
