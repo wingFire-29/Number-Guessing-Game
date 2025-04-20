68f28fbdacc31bf30d7a6a718f9ce5b68c6467d1
wingFire-29 <paragshrivastavadatia@gmail.com>
2025-04-20 18:55:06 +0530

    added a comment

--- game1.c
+++ game1.c
@@ -8,9 +8,11 @@ int main(){
  
         nguess = 0;
         srand(time(0));
-        num=rand()%100;//will genrate random number from 1 to 100
-        //printf("the number is %d\n\n",num);//it will not show which number is generated
+        num=rand()%100;
 
+        //will genrate random number from 1 to 100
+        //printf("the number is %d\n\n",num);//it will not show which number is generated
+        //the number is generated between 0 to 99, so we add 1 to it to get the number between 1 to 100
         do{
             printf("guess the number\n\n");
             scanf("%d",&guess);
